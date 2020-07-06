9a618850da9e5d9df0a66c8afd89ddaf7c1e8cd8
Yosr <yosr.hafsi@esprit.tn>
2020-07-06 14:16:02 +0100

    intialisation du projet

---
+++ src/Claims/ClaimBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Claims\ClaimBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ClaimBundle:Default:index.html.twig');
+    }
+}
