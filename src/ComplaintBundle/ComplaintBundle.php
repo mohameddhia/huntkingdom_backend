53601a2731012955f1c25196718f62ff36606251
Yosr <yosr.hafsi@esprit.tn>
2020-07-10 15:52:22 +0100

    Crud Complaint

---
+++ src/ComplaintBundle/ComplaintBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ComplaintBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ComplaintBundle extends Bundle
+{
+}
---
+++ src/ComplaintBundle/Entity/Type.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ComplaintBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+
+
+
+/**
+ * @ORM\Entity
+ *
+ */
+class Type
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     *@JMS\Serializer\Annotation\Type("string")
+     * @ORM\Column(name="label", type="string", length=255, nullable=true)
+     */
+    private $label;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
